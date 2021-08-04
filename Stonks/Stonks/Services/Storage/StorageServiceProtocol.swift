45a7aa53fc0b7b2f02f0e11b48d85e35e9c57cbf
ILoveEatingFF <vanializogub@gmail.com>
2021-08-04 20:35:15 +0300

    AnyObject

--- Stonks/Stonks/Services/Storage/StorageServiceProtocol.swift
+++ Stonks/Stonks/Services/Storage/StorageServiceProtocol.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-protocol StorageServiceProtocol: class {
+protocol StorageServiceProtocol: AnyObject {
     var currentUser: String? { get }
     func save<T: Encodable>(object: T, for key: String)
     func get<T: Decodable>(for key: String) -> T?
